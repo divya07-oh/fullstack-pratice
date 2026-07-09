e3378717da881e2430c3c5b75e98442e78f8eb32
divya07-oh <divyavenkatesan239@gmail.com>
2026-07-09 21:17:10 +0530

    updating file

---
+++ var.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "teacherbot";
+let botLocation = "The universe";
+let favoriteLanguage = "javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact;
+codingFact = favoriteLanguage + " was created by Brendan Eich. ";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was a interactive language .";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "decides the behaviour of webpage. ";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
