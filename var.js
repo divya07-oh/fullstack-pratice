fd550404e4a528fea64240743611d0a84bc36237
divya07-oh <divyavenkatesan239@gmail.com>
2026-07-10 22:27:18 +0530

    updating file

--- var.js
+++ var.js
@@ -21,3 +21,16 @@ codingFact = favoriteLanguage + "decides the behaviour of webpage. ";
 console.log(codingFact);
 
 console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+console.log("Hi there!");
+
+const botName = "teacherBot";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+
+let sentence = `Today, you will learn about ${topic} in ${subject}.`;
+ console.log(sentence);
